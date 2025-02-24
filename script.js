4c5d6f7c0aba9c4521e93e23b168f26955827f30
script-js <134544514+script-js@users.noreply.github.com>
2025-02-23 16:16:56 -0800

    list organization

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var songs = [];
 var accessToken = localStorage.getItem("accessToken")
 
 async function getList(list) {
-
     var response = await fetch('https://api.spotify.com/v1/playlists/' + list, {
         headers: {
             Authorization: 'Bearer ' + accessToken
@@ -12,8 +11,36 @@ async function getList(list) {
     });
     var data = await response.json();
     if (data.error) {
-      console.error(data.error.message)
+      if (data.error == "Invalid access token.") {
+        localStorage.removeItem("accessToken")
+        location.reload()
+      } else {
+        alert("Error getting playlist: " + data.error.message)
+      }
     } else {
-        console.log(data)
+      console.log(data)
+      playlists[list] = {
+        name: data.name,
+        icon: data.images[0],
+        author: data.owner.display_name,
+        tracks: [],
+        artists: []
+      };
+      data.tracks.items.forEach(function (k) {
+        var id = k.track.uri.replace("spotify:track:","")
+        playlists[list].tracks.push(id)
+        if (!songs.includes(id)) {
+            songs.push(id)
+        }
+        k.track.artists.forEach(function(artist) {
+            var aid = artist.uri.replace("spotify:artist:","")
+            if (!playlists[list].artists.includes(aid)) {
+              playlists[list].artists.push(aid)
+            }
+            if (!artists.includes(aid)) {
+                artists.push(aid)
+            }
+        })
+      });
     }
 }
