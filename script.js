39bb2504626b04b732fdcc598b0eb01cfed3421c
script-js <134544514+script-js@users.noreply.github.com>
2025-02-24 16:15:56 -0800

    ui 1

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var playlists = [];
 var artists = [];
 var songs = [];
 var similar = {
-   artists: [],
-   tracks: [],
-   genres: [] 
+    artists: [],
+    tracks: [],
+    genres: []
 };
 var accessToken = localStorage.getItem("accessToken")
 
@@ -16,52 +16,52 @@ async function getList(list) {
     });
     var data = await response.json();
     if (data.error) {
-      if (data.error == "The access token expired") {
-        localStorage.removeItem("accessToken")
-        spotifyAuth()
-      } else {
-        alert("Error getting playlist: " + data.error.message)
-      }
-    } else {
-      console.log(data)
-      var plist = {
-        name: data.name,
-        icon: data.images[0],
-        author: data.owner.display_name,
-        tracks: [],
-        artists: []
-      };
-      data.tracks.items.forEach(function (k) {
-        var id = k.track.id
-        plist.tracks.push(id)
-        if (!songs.map((x) => x.id).includes(id)) {
-            songs.push({
-                id,
-                artists: k.track.artists.map((x) => x.name).toString().replace(",",", "),
-                icon: k.track.album.images[0].url,
-                title: k.track.name
-            })
+        if (data.error == "The access token expired") {
+            localStorage.removeItem("accessToken")
+            spotifyAuth()
+        } else {
+            alert("Error getting playlist: " + data.error.message)
         }
-        k.track.artists.forEach(function(artist) {
-            if (!plist.artists.includes(artist.id)) {
-              plist.artists.push(artist.id)
-            }
-            if (!artists.map((x) => x.id).includes(artist.id)) {
-                artists.push({
-                    id: artist.id,
-                    url: artist.href
+    } else {
+        console.log(data)
+        var plist = {
+            name: data.name,
+            icon: data.images[0],
+            author: data.owner.display_name,
+            tracks: [],
+            artists: []
+        };
+        data.tracks.items.forEach(function (k) {
+            var id = k.track.id
+            plist.tracks.push(id)
+            if (!songs.map((x) => x.id).includes(id)) {
+                songs.push({
+                    id,
+                    artists: k.track.artists.map((x) => x.name).toString().replace(",", ", "),
+                    icon: k.track.album.images[0].url,
+                    title: k.track.name
                 })
             }
-        })
-      });
-      playlists.push(plist)
+            k.track.artists.forEach(function (artist) {
+                if (!plist.artists.includes(artist.id)) {
+                    plist.artists.push(artist.id)
+                }
+                if (!artists.map((x) => x.id).includes(artist.id)) {
+                    artists.push({
+                        id: artist.id,
+                        url: artist.href
+                    })
+                }
+            })
+        });
+        playlists.push(plist)
     }
 }
 
 function populateSimilar() {
-    songs.forEach(function(s) {
+    songs.forEach(function (s) {
         var isIn = 0;
-        playlists.forEach(function(p) {
+        playlists.forEach(function (p) {
             if (p.tracks.includes(s.id)) {
                 isIn++;
             }
@@ -70,9 +70,9 @@ function populateSimilar() {
             similar.tracks.push(s)
         }
     })
-    artists.forEach(async function(a) {
+    artists.forEach(async function (a) {
         var isIn = 0;
-        playlists.forEach(function(p) {
+        playlists.forEach(function (p) {
             if (p.artists.includes(a.id)) {
                 isIn++;
             }
@@ -88,7 +88,7 @@ function populateSimilar() {
                 name: artistData.name,
                 icon: artistData.images[0].url
             })
-            artistData.genres.forEach(function(g) {
+            artistData.genres.forEach(function (g) {
                 if (!similar.genres.includes(g)) {
                     similar.genres.push(g)
                 }
@@ -98,7 +98,40 @@ function populateSimilar() {
 }
 
 function getScore() {
-   var trackCompat = (similar.tracks.length / (songs.length / 2)) * 100
-   var artistCompat = (similar.artists.length / artists.length) * 100
-   return Math.ceil((trackCompat / 2) + (artistCompat / 2))
+    var trackCompat = (similar.tracks.length / (songs.length / 2)) * 100
+    var artistCompat = (similar.artists.length / artists.length) * 100;
+    var percent = Math.ceil((trackCompat / 2) + (artistCompat / 2))
+    const circularProgress = document.querySelectorAll(".circular-progress");
+
+    Array.from(circularProgress).forEach((progressBar) => {
+        const progressValue = progressBar.querySelector(".percentage");
+        const innerCircle = progressBar.querySelector(".inner-circle");
+        let startValue = 0,
+            speed = 1,
+            progressColor = progressBar.getAttribute("data-progress-color");
+        if (percent) {
+            const progress = setInterval(() => {
+                startValue++;
+                progressValue.textContent = `${startValue}%`;
+                progressValue.style.color = `${progressColor}`;
+
+                progressBar.style.background = `conic-gradient(${progressColor} ${startValue * 3.6
+                    }deg,${progressBar.getAttribute("data-bg-color")} 0deg)`;
+                if (startValue == percent) {
+                    clearInterval(progress);
+                }
+            }, speed);
+        }
+    });
+
+}
+
+function showData() {
+    getScore()
+    songs.forEach(function(s) {
+        var elem = document.createElement("div")
+        elem.classList = "song"
+        elem.innerHTML = `<img class="album-cover" src="${s.icon}" /><span class="title">${s.title}</span><br><span class="artists">${s.artists}</span>`
+        samesongs.appendChild(elem)
+    })
 }
